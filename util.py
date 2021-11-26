563f2719c1689cba2acf4f308c346cbf046f32df
lampysprites <lampysprites@users.noreply.github.com>
2021-11-26 14:52:49 +0700

    Lock created curves

--- util.py
+++ util.py
@@ -287,7 +287,8 @@ class SB_OT_update_spritesheet(bpy.types.Operator, ModalExecuteMixin):
             tag_frames.append(tag_frames[-1]) # one more keyframe to keep the last frame duration inside in the action
 
             if not action.fcurves:
-                action.fcurves.new(f'["Frame {img.name}"]')
+                fcurve = action.fcurves.new(f'["Frame {img.name}"]')
+                fcurve.lock = True
 
             for fcurve in action.fcurves:
                 points = fcurve.keyframe_points
