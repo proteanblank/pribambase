ebcb48fe871954ea14593796faf36067933654c3
lampysprites <lampysprites@users.noreply.github.com>
2021-11-28 01:03:25 +0700

    Get back accidentally removed RunScript

--- client/Commands.lua
+++ client/Commands.lua
@@ -32,8 +32,7 @@ function run_script(f)
     local s = app.fs.joinPath(app.fs.userConfigPath, "extensions", "pribambase", f) .. ".lua"
 
     return function()
-        dofile(s)
-        -- app.command.RunScript{ filename=s }
+        app.command.RunScript{ filename=s }
     end
 end
 
