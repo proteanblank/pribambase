4c66324eddb01e0d55f3b06d489102d58216ace9
lampysprites <lampysprites@users.noreply.github.com>
2022-01-05 01:42:58 +0700

    Bump version

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ bl_info = {
     "author": "lampysprites",
     "description": "Paint pixelart textures in Blender using Aseprite",
     "blender": (2, 80, 0),
-    "version": (2, 1, 0),
+    "version": (2, 1, 1),
     "category": "Paint"
 }
 
